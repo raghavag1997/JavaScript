352f1a843488d403cd4c165c5c8e142fbca2b5be
raghavag1997 <raghav.15jics089@jietjodhpur.ac.in>
2020-04-01 00:42:38 +0530

    Added New Array and object Js file

---
+++ js/arryobj.js
@@ -0,0 +1,28 @@
+let names = ['Raghav','Gaurav','Agarwal']
+let marks = [10,20,40]
+let mixed = ['raghav',10,'Gaurav']
+
+console.log(names)
+
+//concat 
+console.log(names.concat(marks))
+
+
+//Creating Array object with Array class
+
+let name = new Array()
+name=['Shireen']
+console.log(name)
+
+//push and pop
+
+// console.log(marks.push(10))
+// console.log(marks)
+
+marks.pop()
+console.log(marks)
+
+
+//pslice
+
+console.log(names.splice(1,2))
